1963f8c9e5fd94405c60f9d9498820d88a88e1c9
Adwaitsharadmore <amore14@asu.edu>
2025-02-12 18:02:44 -0700

    added logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -33,37 +33,13 @@ export const runtime = 'edge';
      title: "SunHacks",
      content: (
        <div>
-         <p className="text-neutral-800 dark:text-neutral-200 text-xs md:text-sm font-normal mb-8">
-           Built the first ever basic model of Brev. It was when Brev was PrepPal
-         </p>
          <div className="grid grid-cols-2 gap-4">
            <Image
-             src=""
+             src="/images/1.png"
              alt="startup template"
              width={500}
              height={500}
-             className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
-           />
-           <Image
-             src=""
-             alt="startup template"
-             width={500}
-             height={500}
-             className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
-           />
-           <Image
-             src=""
-             alt="startup template"
-             width={500}
-             height={500}
-             className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
-           />
-           <Image
-             src=""
-             alt="startup template"
-             width={500}
-             height={500}
-             className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
+             className="rounded-lg object-cover w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
            />
          </div>
        </div>
@@ -73,43 +49,15 @@ export const runtime = 'edge';
      title: "Early 2023",
      content: (
        <div>
-         <p className="text-neutral-800 dark:text-neutral-200 text-xs md:text-sm font-normal mb-8">
-           I usually run out of copy, but when I see content this big, I try to
-           integrate lorem ipsum.
-         </p>
-         <p className="text-neutral-800 dark:text-neutral-200 text-xs md:text-sm font-normal mb-8">
-           Lorem ipsum is for people who are too lazy to write copy. But we are
-           not. Here are some more example of beautiful designs I built.
-         </p>
          <div className="grid grid-cols-2 gap-4">
            <Image
-             src=""
+             src="/images/2.png"
              alt="hero template"
              width={500}
              height={500}
-             className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
+             className="rounded-lg object-cover w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
            />
-           <Image
-             src=""
-             alt="feature template"
-             width={500}
-             height={500}
-             className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
-           />
-           <Image
-             src=""
-             alt="bento template"
-             width={500}
-             height={500}
-             className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
-           />
-           <Image
-             src=""
-             alt="cards template"
-             width={500}
-             height={500}
-             className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
-           />
+      
          </div>
        </div>
      ),
@@ -118,55 +66,16 @@ export const runtime = 'edge';
      title: "Changelog",
      content: (
        <div>
-         <p className="text-neutral-800 dark:text-neutral-200 text-xs md:text-sm font-normal mb-4">
-           Deployed 5 new components on Aceternity today
-         </p>
-         <div className="mb-8">
-           <div className="flex gap-2 items-center text-neutral-700 dark:text-neutral-300 text-xs md:text-sm">
-             ✅ Card grid component
-           </div>
-           <div className="flex gap-2 items-center text-neutral-700 dark:text-neutral-300 text-xs md:text-sm">
-             ✅ Startup template Aceternity
-           </div>
-           <div className="flex gap-2 items-center text-neutral-700 dark:text-neutral-300 text-xs md:text-sm">
-             ✅ Random file upload lol
-           </div>
-           <div className="flex gap-2 items-center text-neutral-700 dark:text-neutral-300 text-xs md:text-sm">
-             ✅ Himesh Reshammiya Music CD
-           </div>
-           <div className="flex gap-2 items-center text-neutral-700 dark:text-neutral-300 text-xs md:text-sm">
-             ✅ Salman Bhai Fan Club registrations open
-           </div>
-         </div>
+      
          <div className="grid grid-cols-2 gap-4">
            <Image
-             src=""
+             src="/images/3.png"
              alt="hero template"
-             width={500}
-             height={500}
-             className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
+             width={1000}
+             height={1000}
+             className="rounded-lg object-cover w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
            />
-           <Image
-             src=""
-             alt="feature template"
-             width={500}
-             height={500}
-             className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
-           />
-           <Image
-             src=""
-             alt="bento template"
-             width={500}
-             height={500}
-             className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
-           />
-           <Image
-             src=""
-             alt="cards template"
-             width={500}
-             height={500}
-             className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
-           />
+      
          </div>
        </div>
      ),
@@ -406,9 +315,10 @@ const HomePage = () => {
           </div>
 
           <BlurFade delay={0.25} inView>
-            <section id="about" className="pt-[200px]">
+            <section id="about" className="pt-[150px]">
               <motion.div className="relative mx-auto md:items-center justify-center my-50 pb-50">
                 <div className="flex flex-col md:items-center items-center justify-center md:leading-tight">
+                  <Image src="/images/mainlogo1.png" height={100} width={100} alt="logo" />
                   <div className="text-black text-4xl md:text-center text-center sm:text-4xl md:text-5xl lg:text-6xl xl:text-6xl font-semibold leading-tight tracking-tighter font-Inter">
                     Streamline your exam prep with{" "}
                     <span className="text-[#2343fdfa]"> Brev</span>
@@ -418,7 +328,9 @@ const HomePage = () => {
                     achieve more with less stress
                   </div>
                   <div className="z-10 pt-5">
-                    <ShimmerButton onClick={handleStartBreving}>Start Breving</ShimmerButton>
+                    <ShimmerButton onClick={handleStartBreving}>
+                      Start Breving
+                    </ShimmerButton>
                   </div>
                 </div>
               </motion.div>
